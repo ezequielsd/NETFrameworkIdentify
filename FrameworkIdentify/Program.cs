3b98aa80c00732678ec828fa6c75e81002959423
ezequielsd <ezequielsd@live.com>
2017-04-20 18:27:44 -0300

    Adicionado amostragem das informações do sistema.

--- FrameworkIdentify/Program.cs
+++ FrameworkIdentify/Program.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Management;
 using System.Text;
+
 
 namespace FrameworkIdentify
 {
@@ -11,13 +13,56 @@ namespace FrameworkIdentify
     {
         static readonly object _logLockObj = new object();
         private static StringBuilder stbLog = new StringBuilder();
+        private static SystemInfo systemInfo = null;
 
         static void Main(string[] args)
         {
+            SystemInfo system = GetSystemInfo();
+
+          
+            Console.WriteLine(" ");
             stbLog.AppendLine(" ");
+
+            Console.WriteLine("--- " + DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToShortTimeString() +"---- ");
             stbLog.AppendLine("-------- " + DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToShortTimeString() +" -----------");
+
+            Console.WriteLine(" ");
             stbLog.AppendLine(" ");
-            Console.WriteLine("Versões .NET Framework instalados: ");
+
+            Console.WriteLine("Informação do Sistema: ");
+            stbLog.AppendLine("Informação do Sistema: ");
+
+            Console.WriteLine(" ");
+            stbLog.AppendLine(" ");
+
+            Console.WriteLine("Arquitetura: " + system.Archtecture.ToString());
+            stbLog.AppendLine("Arquitetura: " + system.Archtecture.ToString());
+
+            Console.WriteLine("CPU: " + system.CPU);
+            stbLog.AppendLine("CPU: " + system.CPU);
+
+            Console.WriteLine("Domínio: " + system.Domain);
+            stbLog.AppendLine("Domínio: " + system.Domain);
+
+            Console.WriteLine("Nome da máquina: " + system.HostName);
+            stbLog.AppendLine("Nome da máquina: " + system.HostName);
+
+            Console.WriteLine("Memória instalada: " + system.InstalledMemory);
+            stbLog.AppendLine("Memória instalada: " + system.InstalledMemory);
+
+            Console.WriteLine("Localização: " + system.Localization);
+            stbLog.AppendLine("Localização: " + system.Localization);
+
+            Console.WriteLine("Sistema Operacional: " + system.OSName);
+            stbLog.AppendLine("Sistema Operacional: " + system.OSName);
+
+            Console.WriteLine("Sistema Operacional Versão: " + system.OSVersion);
+            stbLog.AppendLine("Sistema Operacional Versão: " + system.OSVersion);
+
+            Console.WriteLine(" ");
+            stbLog.AppendLine(" ");
+
+            Console.WriteLine("Versões .NET Framework instalados: ");            
             stbLog.AppendLine("Versões .NET Framework instalados: ");
 
             Console.WriteLine();
@@ -179,5 +224,125 @@ namespace FrameworkIdentify
                 }
             }
         }
+
+        /// <summary>
+		/// Obtém informações do sistema operacional
+		/// </summary>
+		/// <returns></returns>
+		public static SystemInfo GetSystemInfo()
+        {
+            string sOutput = "";
+
+            try
+            {
+                Process CurrentProcess = new Process();
+                CurrentProcess.StartInfo.FileName = "systeminfo.exe";
+                CurrentProcess.StartInfo.UseShellExecute = false;
+                CurrentProcess.StartInfo.CreateNoWindow = true;
+                CurrentProcess.StartInfo.RedirectStandardOutput = true;
+                CurrentProcess.StartInfo.RedirectStandardInput = true;
+                CurrentProcess.StartInfo.RedirectStandardError = true;
+                CurrentProcess.Start();
+
+                sOutput = CurrentProcess.StandardOutput.ReadToEnd();
+
+                CurrentProcess.WaitForExit(10000);
+                int exitCode = CurrentProcess.ExitCode;
+                CurrentProcess.Close();
+
+                sOutput = sOutput.Replace("\r", "");
+
+                string[] strs = sOutput.Split(new char[] { '\n' });
+                List<string> lines = new List<string>();
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (!string.IsNullOrEmpty(strs[i]) && (strs[i][0] != ' '))
+                        lines.Add(strs[i]);
+                }
+
+                int rColumnWidth = 20;
+                for (int i = rColumnWidth; i < lines[0].Length; i++)
+                {
+                    if (lines[0][i] != ' ') break;
+                    rColumnWidth++;
+                }
+
+                SystemInfo temp = new SystemInfo();
+                temp.Domain = lines[28].Substring(rColumnWidth, lines[28].Length - rColumnWidth).Replace(".local", "");
+                temp.HostName = lines[0].Substring(rColumnWidth, lines[0].Length - rColumnWidth);
+                temp.Localization = lines[19].Substring(rColumnWidth, lines[19].Length - rColumnWidth);
+                temp.OSName = lines[1].Substring(rColumnWidth, lines[1].Length - rColumnWidth);
+                temp.OSVersion = lines[2].Substring(rColumnWidth, lines[2].Length - rColumnWidth);
+                if (lines[13].Substring(rColumnWidth, lines[13].Length - rColumnWidth).Contains("64"))
+                    temp.Archtecture = SystemArchtecture.x64;
+                else
+                    temp.Archtecture = SystemArchtecture.x86;
+
+                temp.InstalledMemory = lines[22].Substring(rColumnWidth, lines[22].Length - rColumnWidth);
+                temp.CPU = GetProcessorID();
+                temp.IsWin2003EnterpriseEditionX86 = temp.OSName.Equals("Microsoft(R) Windows(R) Server 2003, Enterprise Edition") && (temp.Archtecture == SystemArchtecture.x86);
+                systemInfo = temp;
+            }
+            catch
+            {
+               
+            }
+
+            return systemInfo;
+        }
+
+        private static string GetProcessorID()
+        {
+            try
+            {
+                string sCpuInfo = String.Empty;
+
+                //*** Declare Management Class
+                ManagementClass clsMgtClass = new ManagementClass("Win32_Processor");
+                ManagementObjectCollection colMgtObjCol = clsMgtClass.GetInstances();
+
+                //*** Loop Over Objects
+                foreach (ManagementObject objMgtObj in colMgtObjCol)
+                {
+                    //*** Only return cpuInfo from first CPU
+                    if (sCpuInfo == String.Empty)
+                    {
+                        sCpuInfo = objMgtObj.Properties["Name"].Value.ToString();
+                    }
+                }
+
+                return sCpuInfo;
+            }
+            catch
+            {
+                return "unknown";
+            }
+        }
     }
+
+    /// <summary>
+    /// classe representa informações do sistema
+    /// </summary>
+    public class SystemInfo
+    {
+        public string HostName;
+        public string OSName;
+        public string OSVersion;
+        public SystemArchtecture Archtecture;
+        public string Localization;
+        public string Domain;
+        public string InstalledMemory;
+        public string CPU;
+        public bool IsWin2003EnterpriseEditionX86;
+    }
+
+    /// <summary>
+    /// Enumerador define arquitetura do sistema
+    /// </summary>
+    public enum SystemArchtecture
+    {
+        x86,
+        x64
+    }
+
 }
